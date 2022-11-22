52c4ae8471dacb4f7738e1ec7340d48e4292fd43
flavia@flavia-Inspiron-3542 <flavinha.goncalves@gmail.com>
2022-11-21 22:11:24 -0300

    Ajuste entidade loja

--- src/main/java/com/mundotaci/projetotaci/entities/Store.java
+++ src/main/java/com/mundotaci/projetotaci/entities/Store.java
@@ -7,15 +7,20 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Size;
+
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 
-@AllArgsConstructor
+@AllArgsConstructor @Getter @Setter 
 @Entity
 @Table(name = "store")
 public class Store {
 
+    public Store() {
+
+    }
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY) // auto incremento do id
     private Long storeId;
@@ -44,103 +49,4 @@ public class Store {
     @NotBlank
     private String image;
 
-    public String getImage() {
-        return image;
-    }
-
-    public void setImage(String image) {
-        this.image = image;
-    }
-
-    public Store() {
-
-    }
-
-    public Long getStoreId() {
-        return storeId;
-    }
-
-    public void setStoreId(Long storeId) {
-        this.storeId = storeId;
-    }
-
-    public String getCnpj() {
-        return cnpj;
-    }
-
-    public void setCnpj(String cnpj) {
-        this.cnpj = cnpj;
-    }
-
-    public String getNameStore() {
-        return nameStore;
-    }
-
-    public void setNameStore(String nameStore) {
-        this.nameStore = nameStore;
-    }
-
-    public String getAdress() {
-        return adress;
-    }
-
-    public void setAdress(String adress) {
-        this.adress = adress;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getState() {
-        return state;
-    }
-
-    public void setState(String state) {
-        this.state = state;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getCep() {
-        return cep;
-    }
-
-    public void setCep(String cep) {
-        this.cep = cep;
-    }
-
-    public String getOwner() {
-        return owner;
-    }
-
-    public void setOwner(String owner) {
-        this.owner = owner;
-    }
-
-    public String getCelPhone() {
-        return celPhone;
-    }
-
-    public void setCelPhone(String celPhone) {
-        this.celPhone = celPhone;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 }
